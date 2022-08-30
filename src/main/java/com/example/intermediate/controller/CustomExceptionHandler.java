ad62484d046ed1c44ca886fe57cf35c73bec2c52
kimskyyyy <hhhhh823@naver.com>
2022-08-31 02:35:17 +0900

    Multipartfile Null값 일 때 예외상황 응답,  Multipartfile 이미지 크기 10MB이상 일 때  예외상황 응답

--- src/main/java/com/example/intermediate/controller/CustomExceptionHandler.java
+++ src/main/java/com/example/intermediate/controller/CustomExceptionHandler.java
@@ -1,10 +1,12 @@
 package com.example.intermediate.controller;
 
 import com.example.intermediate.controller.response.ResponseDto;
+import org.apache.tomcat.util.http.fileupload.impl.SizeException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.io.IOException;
 @RestControllerAdvice
 public class CustomExceptionHandler {
 
@@ -18,4 +20,13 @@ public class CustomExceptionHandler {
     return ResponseDto.fail("BAD_REQUEST", errorMessage);
   }
 
+  @ExceptionHandler(IOException.class)
+  public ResponseDto<?> IOException() {
+    return ResponseDto.fail("EMPTY", "multipart file is empty");
+  }
+
+  @ExceptionHandler(SizeException.class)
+  public ResponseDto<?> SizeLimitExceededException() {
+    return ResponseDto.fail("CONVERT_FAIL", "fail convert multipart to file");
+  }
 }
