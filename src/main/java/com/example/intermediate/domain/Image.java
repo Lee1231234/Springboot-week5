c5a0d7a8199ff26fcda6a8f4e176cecb783c97cb
kimskyyyy <hhhhh823@naver.com>
2022-08-30 10:36:33 +0900

    domain-image

---
+++ src/main/java/com/example/intermediate/domain/Image.java
@@ -0,0 +1,28 @@
+package com.example.intermediate.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String imgUrl;
+
+}
+
+
+
+
+
